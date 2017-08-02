dce32f4c6a7d2848ca860d864be86904beb5bb1d
Paweł Smoleński <pwl.smolenski@gmail.com>
2017-08-02 23:18:57 +0200

    Scores will be prefixed with zeros

--- app/current-score/current-score.controller.ts
+++ app/current-score/current-score.controller.ts
@@ -1,10 +1,11 @@
 import {Game} from "../domain/game";
+import * as _ from "lodash";
 
 class CurrentScoreController {
 
   constructor(game: Game, container: HTMLDivElement) {
     game.state$.subscribe(state => {
-      container.innerText = state.scores.current.toString();
+      container.innerText = _.padStart(state.scores.current.toString(), 9, '0');
     });
   }
 
